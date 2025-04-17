7e201384c3d15c9ee59792a446b15f4875cfd083
Shubham Jangra <shubhamjangra009@gmail.com>
2025-04-18 03:19:23 +0530

    added compare btn

--- js/menu-btn-script.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.getElementById('menu-btn');
-const verticalNavbar = document.querySelector('.vertical-navbar');
-menuButton.addEventListener('click', function() {
-    verticalNavbar.classList.toggle('vertical-navbar-hidden');
-});
