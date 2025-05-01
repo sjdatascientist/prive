a51d4eb9b221a9a6461c8304e5ac1c283d3fbb26
Shubham Jangra <shubhamjangra009@gmail.com>
2025-05-01 13:19:53 +0530

    .

--- js/membership-btn-script.js
+++ js/membership-btn-script.js
@@ -1,3 +1,7 @@
 function redirectToPaymentForm() {
     window.location.href = window.location.href + '/paymentform';
   }
+
+document.getElementById("new-btn").addEventListener("click", function(e) {
+  document.getElementById('iframe').style.display = "block";
+})
